d345a84c9379294c775113c4e571f229ed4194d3
ole <ole.borchardt@posteo.de>
2024-02-12 15:03:14 +0100

    edited documentation and scripts to run project in a container

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ salloc -N 1 -p clara --gres=gpu:v100:1
 ```
 
 Then run any command with `srun`. To free the resources type `exit` or cancel the job via `scancel`.
+
+To check the status of your submitted jobs run 
+
+```
+squeue -u $USER
+```
+
+## Pull docker image
+
+To log into `ghcr` run 
+
+```
+apptainer remote login --username <USER> docker://ghcr.io
+```
+
+To pull an image run 
+
+```
+singularity pull docker://<ghcr-image>
+```
--- federated.sh
+++ federated.sh
@@ -10,13 +10,21 @@
 #SBATCH --error=logs/%x-%j/stderr.err
 
 CODE_DIR=$HOME/flidp
+CONTAINER_FILE=$CODE_DIR/flidp_main.sif
+
 DATASET_CACHE_DIR=$CODE_DIR/dataset-cache
 
 NUM_CLIENTS=10
 NUM_CPUS=10
 
+echo "START"
 
-cd $CODE_DIR && python3.9 src/main.py \
+singularity exec --nv $CONTAINER_FILE bash -c \
+"cd $CODE_DIR && \
+python3 src/main.py \
     --clients $NUM_CLIENTS \
     --cpus $NUM_CPUS \
     --dataset-cache $DATASET_CACHE_DIR
+"
+
+echo "FINISHED"
