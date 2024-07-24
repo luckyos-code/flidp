ef15982aa38c9640b21ddeae1c77f8361f40e5dd
ole <ole.borchardt@posteo.de>
2024-07-24 12:09:41 +0200

    added notebooks that were used for non-federated experiments

---
+++ src/notebooks/README.md
@@ -0,0 +1,1 @@
+`python -m ipykernel install --user --name flidp-notebooks --display-name "Python (flidp-notebooks)"`
---
+++ src/notebooks/models.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Rescaling, Conv2D, MaxPooling2D, Dropout, Flatten, Dense, GroupNormalization, Resizing
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.applications import EfficientNetV2B0, MobileNetV2
+
+def get_simple_cnn(input_shape, num_classes):
+    model = Sequential([
+        Input(input_shape),
+        Rescaling(1/255.),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Dropout(0.1),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Dropout(0.1),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Dropout(0.1),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dropout(0.1),
+        Dense(num_classes, activation='softmax'),
+    ])
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-3), 
+        loss=SparseCategoricalCrossentropy(), 
+        metrics=['accuracy']
+    )
+    
+    return model
+
+
+def get_lucasnet(input_shape, num_classes):
+    groups = 32
+    model = Sequential([
+        Input(shape=input_shape),
+        Rescaling(1/255.),  # added Rescaling to scale features to [0,1]
+        Resizing(64, 64),  # added resizing to resize to the default value in the original function
+#        RandomFlip(
+#            "horizontal",
+#            seed=42,
+#        ),
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=1,
+            padding="same",
+            activation="relu",
+        ),
+        GroupNormalization(groups=groups),
+        MaxPooling2D(2, 2),
+        Dropout(0.1),  # also added Dropout for regularization
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=1,
+            padding="same",
+            activation="relu",
+        ),
+        GroupNormalization(groups=groups),
+        MaxPooling2D(2, 2),
+        Dropout(0.1),
+        Conv2D(
+            filters=64,
+            kernel_size=(3, 3),
+            strides=1,
+            padding="same",
+            activation="relu",
+        ),
+        GroupNormalization(groups=groups),
+        MaxPooling2D(2, 2),
+        Dropout(0.1),
+        Flatten(),
+        Dense(512, activation="relu"),
+        GroupNormalization(groups=groups),
+        Dropout(0.1),
+        Dense(num_classes, activation="softmax"),
+    ])
+    
+    model.compile(
+        optimizer=Adam(learning_rate=1e-4),
+        loss=SparseCategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
+    
+    return model
+
+
+def get_efficientnet(input_shape, num_classes):
+    model = Sequential([
+        Input(input_shape),
+        Resizing(224, 224),
+        EfficientNetV2B0(
+            include_top=True,
+            weights=None,
+            pooling=None,
+            classes=num_classes,
+            classifier_activation='softmax',
+            include_preprocessing=True
+        )
+    ])
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-3),
+        loss=SparseCategoricalCrossentropy(from_logits=False), 
+        metrics=['accuracy']
+    )
+    
+    return model
+
+
+def get_mobilenet(input_shape, num_classes):
+    model = Sequential([
+        Input(input_shape),
+        Resizing(224, 224),
+        MobileNetV2(
+            classes=num_classes,
+            weights=None,
+            include_top=True,
+        )
+    ])
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-3),
+        loss=SparseCategoricalCrossentropy(from_logits=False), 
+        metrics=['accuracy']
+    )
+    
+    return model
